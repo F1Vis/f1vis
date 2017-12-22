a16974a7e064ad655d0c281764e49ef19dc1f191
Marcel <marcel.felix@hs-hannover.de>
2017-12-22 09:40:26 +0100

    Added the first steps of the course selection

--- js/main.js
+++ js/main.js
@@ -5,6 +5,30 @@
  */
 
 preprocessor.load(function(data) {
+		var $frame  = $('.strecken-selector');
+		var $slidee = $frame.children('ul').eq(0);
+		var $wrap   = $frame.parent();
+    var options = {
+			horizontal: 1,
+			itemNav: 'basic',
+			smart: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 3,
+			scrollBar: $wrap.find('.scrollbar'),
+			scrollBy: 1,
+			pagesBar: $wrap.find('.pages'),
+			activatePageOn: 'click',
+			speed: 300,
+			elasticBounds: 1,
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+    };
+    $('.frame').sly(options);
+
   // Some sample code for a year selector - TODO: Improve a lot and move somewhere else
   var yearSelector = $("#seasonByYearSelector");
   var seasons = processor.getSeasonsWithLapData();
